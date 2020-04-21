bbd267815ccb6b36372d581ddeacbbf3297fe951
cagurl01 <cagurl01@louisville.edu>
2020-04-21 15:37:29 -0400

    Added title and tabindex to h3 elements and trimmed and added events to JS

--- test_scores/dashboard.js
+++ test_scores/dashboard.js
@@ -1,35 +1,20 @@
 'use strict';
+$('.dash-body').hide();
 $('.dash-content').each(function () {
   if (!$(this).text()) {
     $(this).html('<em>none</em>');
   }
 });
-$('.dash-header').click(function () { $(this).next().toggle(); });
-$('.dash-header').on('mouseover mouseout', function () {
-  $(this).parent().toggleClass('dash-ic');
+$('.dash-header').on('click keypress', function () { $(this).next().toggle(); });
+$('.dash-header').on('mouseover focusin', function () {
+  $(this).parent().addClass('dash-ic');
 });
-$('.dash-header').on('mousedown mouseup', function () {
-  $(this).parent().toggleClass('dash-tl');
+$('.dash-header').on('mouseout focusout', function () {
+  $(this).parent().removeClass('dash-ic');
 });
-$('#dash-recdet .dash-body').hide();
-if (window.matchMedia('(max-width: 575px)').matches) {
-  var re = /^\/manage\/lookup\/message\?cmd=compose&person=.+?&recipient=(.+?)(&.+)?$/;
-  $('.dash-popph').each(function () {
-    var matches = $(this).attr('data-href').match(re);
-    if (matches[1]) {
-      $(this).removeAttr('onclick');
-      $(this).attr('href', 'tel:' + matches[1]);
-      $(this).removeAttr('data-href');
-    }
-  });
-  $('.dash-popem').each(function () {
-    var matches = $(this).attr('data-href').match(re);
-    if (matches[1]) {
-      $(this).removeAttr('onclick');
-      $(this).attr('href', 'mailto:' + matches[1]);
-      $(this).removeAttr('data-href');
-    }
-  });
-} else if ($('#dash-tp').text().toLowerCase() !== 'yes') {
-  $('#dash-mob').html($('#dash-mob').text());
-}
+$('.dash-header').on('mousedown', function () {
+  $(this).parent().addClass('dash-tl');
+});
+$('.dash-header').on('mouseup', function () {
+  $(this).parent().removeClass('dash-tl');
+});
